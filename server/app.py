ee7f3ac03008af86b000d7f5df6ae85db498776b
byron korir <byron.korir@student.moringaschool.com>
2024-01-04 15:21:19 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -14,13 +14,54 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        return jsonify(messages), 200, {'Content-Type': 'application/json'}
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
 
-@app.route('/messages/<int:id>')
+        db.session.add(message)
+        db.session.commit()
+
+        return jsonify(message.to_dict()), 201, {'Content-Type': 'application/json'}
+
+@app.route('/messages/<int:id>', methods = [ 'GET', 'PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        message_dict = message.to_dict()
+
+        response = make_response(jsonify(message_dict),200)
+      
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+            db.session.commit()
+
+        message_dict = message.to_dict()
+        response = make_response(jsonify(message_dict), 200)
+
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {"delete_successful":True}
+        response = make_response(jsonify(response_body),200)
+    
+    response.headers['Content-Type'] = 'application/json'
+    return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
